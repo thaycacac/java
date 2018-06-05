a8b66cc9a10b483c9cbaa19cd877360722ebe8e0
thaycacac <thaycacac@gmail.com>
2018-06-05 18:51:16 +0700

    J1.S.P0009

--- LAB211/J1.S.P0009/Fibonacci.java
+++ LAB211/J1.S.P0009/Fibonacci.java
@@ -1,22 +1,15 @@
-package fibonacci;
 
 /**
  *
  * @author THAYCACAC
  */
-public class Fibonacci {
+public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("The 45 sequence fibonacci");
-        System.out.println(fib(45));
+        fibonnaciHelper(45, 1, 0);
     }
 
-    public static int fib(int n) {
-        return fibonnaciHelper(n, 1, 0);
-    }
     public static int fibonnaciHelper(int term, int lower, int higher) {
         if (term < 2) {
             return higher;
@@ -24,4 +17,5 @@ public class Fibonacci {
         System.out.print(higher + " ");
         return fibonnaciHelper(term - 1, higher, higher + lower);
     }
+
 }
