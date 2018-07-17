7444ea52a0b814a5eda703bb8531561f37ee4389
thaycacac <thaycacac@gmail.com>
2018-07-17 09:59:45 +0700

    update some func Q1 practice

--- CSD201/8-KEY PE/Q1_practice.java
+++ CSD201/8-KEY PE/Q1_practice.java
@@ -3,7 +3,7 @@
  * @author Thaycacac
  */
 //add last
-    void addLast(Person x){
+    public void addLast(Person x){
         Node q = new Node(x);
         if(isEmpty()){head=tail=q;return;}
         tail.next=q;tail=q;
@@ -17,16 +17,27 @@
     }
 	
 //add if age > 4
-    void addLastCondition(){
+    public void addLastCondition(){
         Node p=head;
         while(p!=null){
             if(p.info.age>4) h.addLast(p.info);
             p=p.next;
         }
     }
+
+//add element have max age
+    public void addMaxAgeVaoH
+    Person k = getMaxColor();
+    Node p = head;
+    while(p != null){
+        if(p.info.age == k.age){
+            h.addLast(p.info);
+        }
+        p = p.next;
+    }
 	
 //dele first no age < 6
-    void deleteFirstCondition(){
+    public void deleteFirstCondition(){
      Node p = head;
      while(p != null){
          if(p.info.age<6) dele(p);
@@ -34,8 +45,185 @@
      }
     }
 	
+//insert position k
+	public void insertPositionK(Person x, int position) {
+      if (isEmpty()) head = tail = new Node(x);
+      int count = 1;
+      Node p = head;
+      while (p!= null && count < position) {
+          p = p.next;
+          count ++;
+      }
+      Node temp = p.next;
+      p.next = new Node(x, temp);
+	} 
+	
+//insert after position k
+	public void addAfterPositionK(int k, Person c){
+        Node p = new Node(c);
+        if(k == -1){
+            addFirst(c); 
+            return;
+        }
+        
+        int count = 0;
+        Node p1 = head;
+        while(p1 != null && count < k){
+            p1 = p1.next;
+            count++;
+        }
+        
+        if(p1.next == null && count == k){
+            addLast(c);
+            return;
+        }
+            p.next = p1.next;
+            p1.next = p; 
+    }
+	
+//traverse
+    public void traverse(){
+      MyList h=new MyList();
+       Node p=head;Person x;
+       while(p!=null)
+         {x=p.info;h.addFirst(x);
+          p=p.next;
+         }
+       head=h.head;tail=h.tail;
+    }
+	
+//search node
+	public Node search(int x) 
+    {Node p = head;
+      while(p != null && p.info != x) p = p.next;
+      return p;
+	}
+
+//remove a node at position k, starting position 0
+	public void remove(int k) {
+        if (isEmpty()) {
+            return;
+        }
+        if (k == 0) {//check if node is head
+            Node p = head;
+            head = head.next;
+            p.next = null;
+
+        } else {
+            Node p = getNode(k);//get node position k
+            if (p == null) {
+                return;
+            }
+            Node q = getNode(k - 1);//q is node before of p
+            // Remove p
+            q.next = p.next;
+            p.next = null;
+            if (p == tail) {
+                tail = q;
+            }
+        }
+    }
+	//get position k
+	public Node getNode(int k) {
+        int c = 0;
+        Node p = head;
+        while (p != null && c < k) {
+            p = p.next;
+            c++;
+        }
+        return p;
+    }
+
+//remove a node
+    public void remove(Node p){
+        if(p == null || isEmpty()){
+            return;
+        }
+        Node f = null, q = head;
+        // Find the a node before p 
+        while( q!= null && q != p){
+            f = q;
+            q = q.next;
+        }
+        if(f == null){
+            // Remove f
+            head = head.next;
+            if(head == null){
+                tail = null;
+          }
+        }
+        else{
+            f.next = p.next;
+            if(p == tail){
+                tail = f;
+            }
+        }   
+    }
+	
+//remove a node after position k
+    public void removeAfterPosK(int k){
+        if(isEmpty()) return;
+        //remove head
+        if(k == -1) {
+            Node p = head;
+            head = head.next;
+            p.next = null;
+        }else {
+            Node p = getNode(k + 1);
+            if(p == null) return;
+            Node q = getNode(k);
+            //remove p
+            q.next = p.next;
+            p.next = null;
+            if(p == tail) tail = q;
+        }
+    }
+	
+//remove node after 2 node have age < 9
+	public void removeAfterTwoNodeAgeSmallerNine(){
+		Node p = head;
+		while(p != null && p.info.age >= 9){
+			p = p.next;
+		}
+		if(p == null && p.next == null){
+			return;
+		}
+		remove(p.next.next);
+	}
+	
+//remove node thirdth have age < 6
+	public void removeThirthAgeSmallerSix(){
+		Node p = head;
+		int count = 0;
+		while(p != null){
+			if(p.info.age < 6 && count != 3) count ++;
+			else if(p.info.age < 6 && count == 3) break;
+			p = p.next;
+		}
+		remove(p);
+	}
+	
+//remove node first after node have position k
+	public void removeFirstAfterPosition(int k){
+		Node p = getNode(k);
+        if(p == null || p.next == null){//if p is tail then donothing
+            return;
+        }
+        Node q = p.next;//if p is before tail then remove tail
+        if(q.next == null){
+            tail = p;
+            p.next = null;
+        }
+        else{
+            p.next = q.next;
+            q.next = null;
+        }
+	}
+	
+
+	
 //delete node
-	void dele(Node q)
+	public void dele(Node q)
 	 {Node f,p; f=null;p=head;
 	  while(p!=null)
 	   {if(p==q) break;
@@ -78,17 +266,6 @@
            }
          pi=pi.next;
        }
-    }
-	
-//traverse
-    public void traverse(){
-      MyList h=new MyList();
-       Node p=head;Person x;
-       while(p!=null)
-         {x=p.info;h.addFirst(x);
-          p=p.next;
-         }
-       head=h.head;tail=h.tail;
     }
 	
 //append another list
