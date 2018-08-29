b992c0aefe709fc9de210418c4fb841e64960aa9
thaycacac <thaycacac@gmail.com>
2018-08-30 00:32:36 +0700

    add pe example 2

---
+++ PRJ321/5.PE/Sample.PRJ321.SU17/Given/DBContext.java
@@ -0,0 +1,25 @@
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class DBContext {
+    
+    /*USE BELOW METHOD FOR YOUR DATABASE CONNECTION FOR BOTH SINGLE AND MULTILPE SQL SERVER INSTANCE(s)*/
+    /*DO NOT EDIT THE BELOW METHOD, YOU MUST USE ONLY THIS ONE FOR YOUR DATABASE CONNECTION*/
+     public Connection getConnection()throws Exception {
+        String url = "jdbc:sqlserver://"+serverName+":"+portNumber +";databaseName="+dbName;
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        return DriverManager.getConnection(url, userID, password);
+    }
+    
+    /*Insert your other code right after this comment*/
+   
+    /*Change/update information of your database connection, DO NOT change name of instance variables in this class*/
+    private final String serverName = "localhost";
+    private final String dbName = "Northwind";
+    private final String portNumber = "1433";
+    private final String userID = "sa";
+    private final String password = "sa";
+}
